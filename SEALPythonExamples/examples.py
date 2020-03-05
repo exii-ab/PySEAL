b92d7761ec8737c69fe46c094e0b65c652e11b1b
Andre Bertolace <ab@exindiciis.com>
2020-03-05 10:55:26 +0100

    translating example2 into python

--- SEALPythonExamples/examples.py
+++ SEALPythonExamples/examples.py
@@ -1,5 +1,10 @@
 from example_1_bfv_basics import example_bfv_basics
+from example_2_encoders import example_integer_encoder, example_batch_encoder, example_ckks_encoder
 
 if __name__ == '__main__':
     print("=================== Running example 1: BFV Basics ====================")
     example_bfv_basics()
+    print("=================== Running example 2: Encoders ====================")
+    example_integer_encoder()
+    example_batch_encoder()
+    example_ckks_encoder()
