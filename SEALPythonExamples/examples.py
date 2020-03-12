e367c8b77677cdf311b279669fd43de491e05b58
Andre Bertolace <ab@exindiciis.com>
2020-03-12 11:10:44 +0100

    Translating example3 (levels) to python code

--- SEALPythonExamples/examples.py
+++ SEALPythonExamples/examples.py
@@ -1,5 +1,6 @@
 from example_1_bfv_basics import example_bfv_basics
 from example_2_encoders import example_integer_encoder, example_batch_encoder, example_ckks_encoder
+from example_3_levels import example_levels
 
 if __name__ == '__main__':
     print("=================== Running example 1: BFV Basics ====================")
@@ -8,3 +9,5 @@ if __name__ == '__main__':
     example_integer_encoder()
     example_batch_encoder()
     example_ckks_encoder()
+    print("=================== Running example 3: Levels ====================")
+    example_levels()
